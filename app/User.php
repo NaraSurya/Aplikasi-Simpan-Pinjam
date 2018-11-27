718a2434990a0de82d9b763a8d622f8cb102f0d2
dyatmika12 <dyatmika12@gmail.com>
2018-11-27 23:04:25 +0800

    user things

--- app/User.php
+++ app/User.php
@@ -15,9 +15,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['nama', 'email', 'password', 'user_role', 'username', 'password', 'nik'];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -27,4 +25,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function anggota(){
+        return $this->hasMany('App\Anggota');
+    }
+
 }
